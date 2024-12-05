7413bff8922009fff22a703c3b8088d6d1e9b9cf
Boodenator <boode1000@gmail.com>
2024-12-05 00:14:04 -0800

    island-param-function

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Island Perimeter:
+    will return perimeter of island demonstrated in GRID
+"""
+
+
+def island_perimeter(grid):
+    """island perimenter fn."""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
